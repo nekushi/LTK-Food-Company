63ed82fc62f89abaffdebaa58e4daf76dd33700b
Ivan <i.pabayo3000@gmail.com>
2026-02-04 10:54:36 +0800

    ADDED get and post user from/to the db

---
+++ dal/user.ts
@@ -0,0 +1,77 @@
+"use server";
+
+import { Prisma, Role } from "@/app/generated/prisma/client";
+import prisma from "@/lib/db";
+
+export async function addUser(prevState: unknown, formdata: FormData) {
+  const firstName = formdata.get("firstName") as string;
+  const lastName = formdata.get("lastName") as string;
+  const username = formdata.get("username") as string;
+  const password = formdata.get("password") as string;
+  const role = formdata.get("role") as Role;
+
+  return await prisma.$transaction(async (ctx) => {
+    const user = await ctx.user.create({
+      data: {
+        firstName,
+        lastName,
+        username,
+        password,
+        role,
+      },
+    });
+
+    if (user.role === Role.EMPLOYEE) {
+      await ctx.employee.create({
+        data: {
+          userId: user.id,
+        },
+      });
+    } else if (user.role === Role.DELIVERY_PERSONNEL) {
+      await ctx.deliveryPersonnel.create({
+        data: {
+          userId: user.id,
+        },
+      });
+    } else if (user.role === Role.STOCK_MANAGER) {
+      await ctx.stockManager.create({
+        data: {
+          userId: user.id,
+        },
+      });
+    } else if (user.role === Role.HUMAN_RESOURCE) {
+      await ctx.humanResource.create({
+        data: {
+          userId: user.id,
+        },
+      });
+    }
+
+    console.log(user);
+    return user;
+  });
+
+  const user = await prisma.user.create({
+    data: {
+      firstName,
+      lastName,
+      username,
+      password,
+      role,
+    },
+  });
+
+  if (user.role === "EMPLOYEE") {
+    const employeeUser = await prisma.employee.create({
+      data: {
+        userId: user.id,
+      },
+    });
+  } else if (user.role === "STOCK_MANAGER") {
+    const stockManagerUser = await prisma.stockManager.create({
+      data: {
+        userId: user.id,
+      },
+    });
+  }
+}
