076ce232eb18a7d8594e1a2a38506c2f36b49b75
Ivan <i.pabayo3000@gmail.com>
2026-02-05 23:06:26 +0800

    MODIFIED add user form

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,19 @@
 
 import Link from "next/link";
 import { addUser } from "@/dal/user";
-import { useActionState } from "react";
+import { useState, useActionState } from "react";
 
 export default function LoginPage() {
+  const [isRoleEmployee, setIsRoleEmployee] = useState<boolean>(true);
   const [state, addUserAction, isPending] = useActionState(addUser, undefined);
+
+  const handleRoleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value === "EMPLOYEE") {
+      setIsRoleEmployee(true);
+    } else {
+      setIsRoleEmployee(false);
+    }
+  };
 
   return (
     <div className="p-4">
@@ -20,15 +29,17 @@ export default function LoginPage() {
         className="p-4 border rounded-2xl flex flex-col w-1/4 *:border *:rounded *:px-2 *:py-1 space-y-2"
       >
         <h2 className="text-xl">Add user</h2>
-        First Name: <input type="text" name="firstName" />
+        First Name:
+        <input type="text" name="firstName" />
         Last Name:
         <input type="text" name="lastName" />
-        Username:
-        <input type="text" name="username" />
-        Password
-        <input type="text" name="password" />
         Role:
-        <select name="role" id="browser" defaultValue={"employee"}>
+        <select
+          name="role"
+          id="browser"
+          defaultValue={"EMPLOYEE"}
+          onChange={handleRoleChange}
+        >
           {/* <option value="ADMIN">Admin</option> */}
           <option value="HUMAN_RESOURCE">Human Resource</option>
           <option value="STOCK_MANAGER">Stock Manager</option>
@@ -36,6 +47,19 @@ export default function LoginPage() {
           <option value="COLLABORATOR">Collaborator</option>
           <option value="EMPLOYEE">Employee</option>
         </select>
+        {isRoleEmployee ? (
+          <>
+            <p>Employee ID:</p>
+            <input type="text" name="employeeId" />
+          </>
+        ) : (
+          <>
+            Username:
+            <input type="text" name="username" />
+            Password
+            <input type="text" name="password" />
+          </>
+        )}
         <button type="submit" className="hover:bg-blue-100">
           Add user
         </button>
