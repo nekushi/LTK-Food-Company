c8bc4a176502dccc14f16cfb9292cf2a072b07c7
Ivan <i.pabayo3000@gmail.com>
2026-02-08 15:40:56 +0800

    FIXED excel cell date format on display

--- app/dashboard/human-resource/workData.tsx
+++ app/dashboard/human-resource/workData.tsx
@@ -1,10 +1,12 @@
 import { linkToEmployee } from "@/dal/geo";
+import { InOut, TypeAttendanceCard, TypeSchedules, Schedule } from "@/index";
+import { cellDataToTime, normalizeTime } from "@/utils/excelTImeFormat";
 
 export default function EmployeeWorkData({
   data,
   onApprovedData,
 }: {
-  data: any;
+  data: TypeAttendanceCard;
   onApprovedData: any;
 }) {
   // const handleLinkDataClick = async () => {
@@ -13,6 +15,11 @@ export default function EmployeeWorkData({
   //   const res = await linkToEmployee(data);
   //   console.log(res);
   // };
+
+  // console.log(data.schedules);
+
+  // const newasd = data.schedules.map((asd: any) => asd);
+  // console.log(newasd);
 
   return (
     <>
@@ -36,17 +43,44 @@ export default function EmployeeWorkData({
         <p>Charges: {data.charges}</p>
         <p>Real Pay: {data.realPay}</p>
         <p>Device ID: {data.deviceId}</p>
-        <p>Schedules: XD</p>
-        {/* <div>
-          Schedules: <pre>{JSON.stringify(data.schedules)}</pre>
-        </div> */}
-        <p>Employee Signature: {data.employeeSignature}</p>
-        <button
+        <p>Device ID: {data.employeeSignature}</p>
+        {/* <p>Schedules: XD</p> */}
+        <div>
+          Schedules:{" "}
+          {data.schedules.map((asd: TypeSchedules, i: number) => {
+            <pre key={i}>{JSON.stringify(asd, null, 2)}</pre>;
+            const [dateId, schedule] = Object.entries(asd)[0] as [
+              string,
+              Schedule,
+            ];
+
+            return (
+              <div key={dateId}>
+                <div>
+                  Morning In:{" "}
+                  {normalizeTime(schedule.in_out.morning.morning_in)}
+                  {/* {schedule.in_out.morning.morning_in} */}
+                  {/* {String(schedule.in_out.morning.morning_in).length === 6 &&
+                  String(schedule.in_out.morning.morning_in) === "      "
+                    ? "---"
+                    : String(schedule.in_out.morning.morning_in).length !== 6 &&
+                        /^\d{2}:\d{2}$/.test(
+                          String(schedule.in_out.morning.morning_in),
+                        )
+                      ? schedule.in_out.morning.morning_in
+                      : cellDataToTime(schedule.in_out.morning.morning_in)} */}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        {/* <p>Employee Signature: {(data.employeeSignature["10:01"], null, 2)}</p> */}
+        {/* <button
           onClick={() => onApprovedData(data)}
           className="px-2 py-1 border rounded hover:bg-blue-200 active:bg-blue-100"
         >
           Link to {data.name}
-        </button>
+        </button> */}
       </div>
     </>
   );
