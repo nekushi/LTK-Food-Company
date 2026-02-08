edda13cc503056153a4d1018aa474c21602f6220
Ivan <i.pabayo3000@gmail.com>
2026-02-08 22:54:47 +0800

    ADDED clear form in upload excel

--- app/dashboard/human-resource/excel.tsx
+++ app/dashboard/human-resource/excel.tsx
@@ -41,23 +41,44 @@ export default function ExcelForm() {
     postExcelFile(results, employeeId);
   };
 
+  const handleClearForm = () => {
+    setExcelFile([]);
+  };
+
   return (
     <div className="">
       <br />
-      <div>Enter excel file</div>
-      <input
-        type="file"
-        onChange={handleUpload}
-        className="border border-black rounded my-4"
-      />
-      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
-      {excelFile.map((data: any) => (
-        <EmployeeWorkData
-          key={data.id}
-          data={data}
-          onApprovedData={handleApprovedData}
+      <div>Upload excel file</div>
+      <form action="" className="flex flex-row">
+        <input
+          type="file"
+          onChange={handleUpload}
+          className="
+            block w-auto text-sm text-gray-500
+            file:mr-4 file:py-2 file:px-4
+            file:rounded-md file:border-0
+            file:text-sm file:font-semibold
+            file:bg-blue-50 file:text-blue-700
+            hover:file:bg-blue-100
+          "
         />
-      ))}
+        <button
+          type="reset"
+          onClick={handleClearForm}
+          className="py-1 px-4 bg-red-300 border-red-300 rounded-md hover:bg-red-400 active:bg-red-500 transition"
+        >
+          Clear upload
+        </button>
+      </form>
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      {excelFile.length !== 0 &&
+        excelFile.map((data: any) => (
+          <EmployeeWorkData
+            key={data.id}
+            data={data}
+            onApprovedData={handleApprovedData}
+          />
+        ))}
       <button
         onClick={handleLinkDataClick}
         className="border px-2 py-1 rounded"
