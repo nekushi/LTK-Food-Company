a9b95229623f9e1abdc6254ea7c63b477220a4f5
Ivan <i.pabayo3000@gmail.com>
2026-02-08 23:02:27 +0800

    CREATED hr dashboard page. nothing special

--- app/dashboard/human-resource/page.tsx
+++ app/dashboard/human-resource/page.tsx
@@ -1,23 +1,7 @@
-import Link from "next/link";
-import ExcelForm from "./excel";
-
 export default function HumanResource() {
   return (
-    <div className="p-4">
-      <div>
-        <p>This is human resource page.</p>
-        <Link
-          href={"/dashboard/inventory"}
-          className="border py-1 px-2 rounded"
-        >
-          Go to Inventory
-        </Link>
-        <Link href={"/dashboard/Delivery"} className="border py-1 px-2 rounded">
-          Go to Delivery
-        </Link>
-      </div>
-
-      <ExcelForm />
+    <div>
+      <h2>This is HR Dashboard</h2>
     </div>
   );
 }
