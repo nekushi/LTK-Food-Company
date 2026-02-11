10505a408381105e246d94275e48c64e09f4cb20
Ivan <i.pabayo3000@gmail.com>
2026-02-11 16:16:15 +0800

    TYPESAFED dateFomatter. checkpoint before unit testing

--- utils/excelTImeFormat.ts
+++ utils/excelTImeFormat.ts
@@ -1,32 +1,24 @@
-export function cellDataToTime(time: any): any {
-  if (typeof time !== "number" || Number.isNaN(time)) return time;
+export function cellDataToTime(time: number): string {
+  // if (typeof time !== "number" || Number.isNaN(time)) return time;
 
   const totalSeconds = Math.round(time * 24 * 60 * 60);
 
   const hours = Math.floor(totalSeconds / 3600);
   const minutes = Math.floor((totalSeconds % 3600) / 60);
 
-  //   console.log(time);
-  //   const excelTime = time * 24 * 60;
-  //   console.log(excelTime);
-  //   const hours = Math.floor(excelTime / 60);
-  //   console.log(hours);
-  //   const minutes =
-  //     // Math.round(excelTime % 60) === 60 ? 0 : Math.round(excelTime % 60);
-  //     // excelTime % 60;
-  //     Math.floor((excelTime % 3600) / 60);
-  //   console.log(minutes);
-
   const formatTime = `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}`;
   return formatTime;
 }
 
-export function normalizeTime(value: any) {
+export function normalizeTime(value: number | null): string {
   if (value == null) return "---";
 
   const raw = String(value);
 
   if (/^\s+$/.test(raw)) return "---";
+
+  // if (typeof value !== "number" || Number.isNaN(value)) return String(value);
+  if (typeof value !== "number" || Number.isNaN(value)) return raw;
 
   if (/^\d{2}:\d{2}$/.test(raw)) return raw;
 
@@ -42,6 +34,8 @@ export function normalizeTime(value: any) {
     const [h, m] = raw.split(":");
     return `${h.padStart(2, "0")}:${m}`;
   }
+
+  // if (typeof value !== "number" || Number.isNaN(value)) return String(value);
 
   return "---";
 }
